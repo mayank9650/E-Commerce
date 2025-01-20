3c81655d95a78d3df8df0913edb4d324d16c660e
Mayank Bhandari <mayankbhandari9650@gmail.com>
2025-01-20 23:38:42 +0530

    Filters and sorting added

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,42 +1,36 @@
-import React from 'react'
-import Button from '../../utils/atoms/button/Button'
-import SingleSelect from '../../utils/molecules/singleselect/SingleSelect'
-import { TEXT_CONSTANS } from '../../utils/constants';
+import React from "react";
+import Button from "../../utils/atoms/button/Button";
+import SingleSelect from "../../utils/molecules/singleselect/SingleSelect";
+import { TEXT_CONSTANS } from "../../utils/constants";
 
-export default function Filter({
-    updateFilters,
-    filterData,
-    categoryOptions
-}) {
+import './filter.css'
 
-    const handleConfirm = (selected) => {
-        updateFilters('category', selected)
-    };
+export default function Filter({ updateFilters, filterData, categoryOptions }) {
+  const handleConfirm = (selected) => {
+    updateFilters("category", selected);
+  };
 
-    const handleCancel = () => {
-        console.log('Selection cancelled');
-    };
-
-    const sortHandler = () => {
-        let sorting = 'asc'
-        if(filterData.sort === 'asc'){
-            sorting = 'desc'
-        }
-        updateFilters('sort', sorting)
+  const sortHandler = () => {
+    let sorting = "asc";
+    if (filterData.sort === "asc") {
+      sorting = "desc";
     }
+    updateFilters("sort", sorting);
+  };
 
-    return (
-        <div>
-            <h2>{TEXT_CONSTANS.FILTERS}:</h2>
-            <SingleSelect
-                options={categoryOptions}
-                onConfirm={handleConfirm}
-                onCancel={handleCancel}
-            />
-            <Button 
-                text={`${TEXT_CONSTANS.SORT}: ${filterData.sort ==='asc' ? TEXT_CONSTANS.LOW_TO_HIGHT : TEXT_CONSTANS.HIGH_TO_LOW}`} 
-                onClick={sortHandler}
-            ></Button>
-        </div>
-    )
+  return (
+    <div className="filter-section">
+      <h2>{TEXT_CONSTANS.FILTERS}:</h2>
+      <SingleSelect options={categoryOptions} onConfirm={handleConfirm} />
+      <Button
+        text={`${TEXT_CONSTANS.SORT}: ${
+          filterData.sort === "asc"
+            ? TEXT_CONSTANS.LOW_TO_HIGHT
+            : TEXT_CONSTANS.HIGH_TO_LOW
+        }`}
+        onClick={sortHandler}
+        className={'sort-btn'}
+      ></Button>
+    </div>
+  );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Navbar from '../components/navbar/Navbar'
-import Products from '../components/products/Products'
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+import Products from "../components/products/Products";
 
 export default function Dashboard() {
+  // Additional functionality can be added like footer
   return (
     <>
-        <Navbar></Navbar>
-        <main>
-            <Products></Products>
-        </main>
+      <Navbar></Navbar>
+      <main>
+        <Products></Products>
+      </main>
     </>
-  )
+  );
 }
